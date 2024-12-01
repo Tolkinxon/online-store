51aa07aae5654c476f25bd49b2eb770277dbae0b
Soliyev Tolkinxon <krosstony639@gmail.com>
2024-12-02 01:00:57 +0500

    Finish bookmark

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,57 @@
-const token = localStorage.getItem('token') ? JSON.stringify(localStorage.getItem('token')):"";
+const elItemTemplate = document.querySelector('.js-item-template').content;
+const elList = document.querySelector('.js-list');
 
-if(!token) window.location = '../../index.html'
+const token = localStorage.getItem('token');
+const bookmarked = localStorage.getItem('bookmarked') ? JSON.parse(localStorage.getItem('bookmarked')) : [];
+let data = [];
 
-;(async () => {
-    const req = await fetch('http://localhost:5000/user/register',{
-        method: 'GET',
+if(!token) window.location = '/index.html'
+
+function render(arr, node) {
+    node.innerHTML = '';
+    const fragment = document.createDocumentFragment();
+    
+    arr.forEach(({product_img, product_name, product_desc, product_price, id}) => {
+        const clone = elItemTemplate.cloneNode(true);
+
+        clone.querySelector('.js-item').dataset.id = id;
+        clone.querySelector('.js-product-img').src = `http://localhost:5000/${product_img}`;
+        clone.querySelector('.js-product-name').textContent = product_name;
+        clone.querySelector('.js-product-description').textContent = `${product_desc.slice(0, 20)}...`;
+        clone.querySelector('.js-product-price').textContent = `$${product_price}`;
+        clone.querySelector('.js-bookmark-galochka').style.display = bookmarked.some(item => item == id) ? 'block' : 'none'
+
+        fragment.append(clone);
+    });
+    node.append(fragment);
+}
+
+ ;(async () => {
+    const req = await fetch('http://localhost:5000/product',{
+        method: "GET",  
         headers: {
             authorization: token
         }
     });
     const res = await req.json();
-    console.log(res);
-})()
+    data = res
+    render(res, elList);
+})();
+
+
+
+elList.addEventListener('click', (evt)=>{
+    function findItem (evt) {
+        if(!evt.matches('.js-item')) return findItem(evt.parentElement)
+        else { 
+           return evt.dataset.id
+        }
+    }
+
+    const id = findItem(evt.target);
+    const bookmarkBtn = evt.target.matches('.js-bookmark-btn');
+    const buyBtn = evt.target.matches('.js-buy-btn');
+
+    if(!(bookmarked.includes(id)) && bookmarkBtn) {bookmarked.push(id); render(data, elList); return}
+    if((bookmarked.includes(id)) && bookmarkBtn)  {bookmarked.splice(bookmarked.findIndex(item => item == id), 1); render(data, elList); return}
+})
--- src/js/signup.js
+++ src/js/signup.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.js-form');
 
-const token = localStorage.getItem('token') ? JSON.stringify(localStorage.getItem('token')):'';
-if(token) window.location = './../html/main.html';
+const token = localStorage.getItem('token');
+if(token) window.location = '/src/html/main.html';
 
 const reginster = async (data) => {
         const req = await fetch('http://localhost:5000/user/register',{
@@ -11,7 +11,7 @@ const reginster = async (data) => {
     const res = await req.json();
     if(res.token){
         localStorage.setItem('token', res.token);
-        window.location = './src/html/main.html'
+        window.location = '/src/html/main.html'
     } else { alert('Something went wrong please input correct information!'); }
 }
 
